c2152d81784459ee75f63b4b779344d5451ba6f2
Team-techno <103729598+Team-techno@users.noreply.github.com>
2022-08-02 18:16:08 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -10,10 +10,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def start_(client: Client, message: Message):
     await message.delete()
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1ebc3393692680b98c7ef.jpg",
+        photo=f"https://telegra.ph/file/a8f18df4770b2336d7fde.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━
 ∆ ʜᴇʏ {message.from_user.mention()} !
-        ᴛʜɪs ɪs [misscutiemusic](t.me/BROKEN_MUSIC_ROBOT),  ɪ ᴀᴍ ᴀ ᴍᴜsɪᴄ ʙᴏᴛ, ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ʏᴏᴜʀ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ....
+        ᴛʜɪs ɪs [misscutiemusic](t.me/Misscutiemusicbot),  ɪ ᴀᴍ ᴀ ᴍᴜsɪᴄ ʙᴏᴛ, ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ʏᴏᴜʀ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ....
 ᴀʟʟ ᴏꜰ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʜᴀɴᴅʟᴇʀs : ( `/ . • $ ^ ~ + * ?` )
 ┏━━━━━━━━━━━━━━┓
 ┣★
